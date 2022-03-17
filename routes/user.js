b74129082461c2f83d4ddf4494f8a182c6dab93f
Asim-Shaik <75205032+Asim-Shaik@users.noreply.github.com>
2022-03-17 07:58:18 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -3,13 +3,14 @@ import express from "express";
 import {
   getUser,
   createUser,
+  getAllUser,
   deleteUser,
   updateUser,
 } from "../controllers/user.js";
 
 const routerUser = express.Router();
 
-routerUser.route("/").post(createUser);
+routerUser.route("/").get(getAllUser).post(createUser);
 routerUser.route("/:id").get(getUser).delete(deleteUser).patch(updateUser);
 
 export default routerUser;
